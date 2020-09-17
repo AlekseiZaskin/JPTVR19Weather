00be1c6f4dde35d9605e9394dddd6dbf9a7ddbcf
pupil <pupil@ivkhk.ee>
2020-09-17 10:02:10 +0300

    Заготовка проекта

---
+++ src/jptvr19weather/App.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jptvr19weather;
+
+/**
+ *
+ * @author pupil
+ */
+class App {
+    publik void run(){
+            System.out.println("Hello!");
+}
